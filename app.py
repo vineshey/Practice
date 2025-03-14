3f2e90e144cc97824bf2d53c1a9477a88f156e22
VJ13SS <viswajith5749@gmail.com>
2025-03-14 21:46:19 +0530

    updated the functionalities for deployments on vercel

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def getHackathons():
     
     return results
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",10000)))
